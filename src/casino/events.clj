baed25016a392432fa43d153cb1aa7bcd5422b5c
Joshua Suskalo <joshua@suskalo.org>
2020-06-12 10:09:04 -0500

    Update ready event to store the user under :bot/user

--- src/casino/events.clj
+++ src/casino/events.clj
@@ -8,7 +8,7 @@
 (defn store-user
   "Stores the user object for the bot in the [[state]] atom."
   [ready-event {user :user}]
-  (swap! state assoc :user user))
+  (swap! state assoc :bot/user user))
 
 (defn make-run-commands
   "Makes an event handler which evaluates the given commands.
