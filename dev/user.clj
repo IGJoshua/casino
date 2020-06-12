a0d7ec52f34f677e511ad8ea1ed73919bc2e88ce
Joshua Suskalo <joshua@suskalo.org>
2020-06-11 22:17:03 -0500

    Reorder requires in user code

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,9 @@
 (ns user
   (:require
+   [casino.house :as h]
    [clojure.core.async :as a]
    [clojure.java.io :as io]
-   [clojure.string :as str]
-   [casino.house :as h]))
+   [clojure.string :as str]))
 
 (def token (str/trim (slurp (io/resource "token-canary.txt"))))
 
