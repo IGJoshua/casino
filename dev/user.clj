6935e5bad35686a15313c0c80958321b138a8dfa
Joshua Suskalo <joshua@suskalo.org>
2020-06-11 20:44:57 -0500

    Add functions to read the token in and run the bot

--- dev/user.clj
+++ dev/user.clj
@@ -1,3 +1,13 @@
 (ns user
   (:require
+   [clojure.core.async :as a]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
    [casino.house :as h]))
+
+(def token (str/trim (slurp (io/resource "token-canary.txt"))))
+
+(defn run-bot
+  "Starts the bot with the canary token on a dedicated thread."
+  []
+  (a/thread (h/run token)))
--- src/casino/house.clj
+++ src/casino/house.clj
@@ -1,7 +1,15 @@
 (ns casino.house
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str])
   (:gen-class))
 
+(defn run
+  "Starts a bot using the given `token`."
+  [token])
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Starts a bot with the main token, shuts down agents once it stops."
   [& args]
-  (println "Hello, World!"))
+  (run (str/trim (slurp (io/resource "token.txt"))))
+  (shutdown-agents))
